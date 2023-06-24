a9eb730afd6d0c4d120eeff6b7c3c986d561ae13
techcursed <31003460+TechCursed@users.noreply.github.com>
2023-06-24 17:23:19 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Contacts-Crud-App-React-Redux
+ 
---
+++ src/components/AppNavbar.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { searchUser } from "../features/userDetailSlice";
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+const AppNavbar = () => {
+  const allusers = useSelector((state) => state.app.users);
+  const dispatch = useDispatch();
+
+  const [searchData, setSearchData] = useState("");
+
+  useEffect(() => {
+    dispatch(searchUser(searchData));
+  }, [searchData]);
+
+  return (
+    <div>
+
+    <Navbar expand="lg" className="bg-body-tertiary bg-light">
+    <Container fluid>
+      <Navbar.Brand href="#">Contacts App</Navbar.Brand>
+      <Navbar.Toggle aria-controls="navbarScroll" />
+      <Navbar.Collapse id="navbarScroll">
+        <Nav
+          className="me-auto my-2 my-lg-0"
+          style={{ maxHeight: '100px' }}
+          navbarScroll
+        >
+          <Nav.Link href="/read">All Contacts ({allusers.length}) </Nav.Link>
+          <Nav.Link href="/create">Create Contact</Nav.Link>
+        </Nav>
+        <Form className="d-flex">
+          <Form.Control
+            type="search"
+            placeholder="Search"
+            className="me-2"
+            aria-label="Search"
+            value={searchData}
+            onChange={(e) => setSearchData(e.target.value)}
+          />
+        </Form>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+
+</div>
+  );
+};
+
+export default AppNavbar;
